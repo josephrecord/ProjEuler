7bda596537307941aa0a187ceb1a5648ee8a78e2
josephrecord <joseph.record@gmail.com>
2015-08-05 21:46:43 -0400

    Soln to prob 5

--- p5.py
+++ p5.py
@@ -1,32 +1,3 @@
-##import math
-##
-##def gcd(a,b):
-##
-##    if a > b:
-##        c = b
-##        b = a
-##        a = c
-##
-##    rem = 1
-##
-##    while rem != 0:
-##        rem = b%a
-##        b = a
-##        a = rem
-##        if rem == 0:
-##            gcd = b
-##
-##    return gcd
-##
-##
-##def lcm(a,b):
-##
-##    return a / gcd(a,b) * b
-##
-##
-##def lcmm(*args):
-##    return reduce(lcm, args)
-
 import functools
 
 def gcd(a, b):
