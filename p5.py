c32baf940759d3b338db2f24ff729a4a1dccb16e
josephrecord <joseph.record@gmail.com>
2015-08-05 21:38:04 -0400

    Soln to prob 5

--- p5.py
+++ p5.py
@@ -1,34 +1,47 @@
-import math
+##import math
+##
+##def gcd(a,b):
+##
+##    if a > b:
+##        c = b
+##        b = a
+##        a = c
+##
+##    rem = 1
+##
+##    while rem != 0:
+##        rem = b%a
+##        b = a
+##        a = rem
+##        if rem == 0:
+##            gcd = b
+##
+##    return gcd
+##
+##
+##def lcm(a,b):
+##
+##    return a / gcd(a,b) * b
+##
+##
+##def lcmm(*args):
+##    return reduce(lcm, args)
 
-def gcd(a,b):
+import functools
 
-    if a > b:
-        c = b
-        b = a
-        a = c
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:      
+        a, b = b, a % b
+    return a
 
-    rem = 1
-
-    while rem != 0:
-        rem = b%a
-        b = a
-        a = rem
-        if rem == 0:
-            gcd = b
-
-    return gcd
-
-
-def lcm(a,b):
-
-    return a / gcd(a,b) * b
-
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
 
 def lcmm(*args):
-    return reduce(lcm, args)
-
-
-
+    """Return lcm of args."""   
+    return functools.reduce(lcm, args)
 
 
 
